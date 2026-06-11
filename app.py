5f9420a178d2f18e55410e2794feb5c5a64a6684
Ashwini1813 <ashwiniparmar13195@gmail.com>
2026-06-11 12:36:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ if st.button("🎯 Predict Math Score"):
     # Show result
     st.markdown(f"""
     <div style='
-        background: linear-gradient(135deg, #ff6fd8, #3813c2);
+        background: linear-gradient(135deg, #00d4ff, #090979);
         padding: 20px;
         border-radius: 15px;
         text-align: center;
